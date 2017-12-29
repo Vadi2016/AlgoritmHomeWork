ddfe10ee1f0bab0ec1348f1fb1d0059c12c74109
Vadi2016 <vadi2016@outlook.com>
2017-12-29 13:22:29 +0300

    Add files via upload

---
+++ HomeWorkLessonSeven/homework7.h
@@ -0,0 +1,32 @@
+﻿
+
+
+// Тут код
+int* readFileToMatrix(FILE *file, int *size) {
+    	if (file == NULL) {
+        	puts("Can't open file!");
+    	}
+    	fscanf(file, "%d", size); // Считываем количество записей
+    	int *arr;
+    	arr = (int*)malloc(*size * *size * sizeof(int));
+	//    int arr[size][size];
+    	for(int i = 0; i < *size; i++) {
+        	for (int j = 0; j < *size; j++) {
+            		fscanf(file, "%d", (arr + i * *size + j));
+        	}
+    	}
+	
+    	fclose(file);
+	
+    	return arr;
+}
+
+void printMatrix(int **arr, int size) {
+    	for (int i = 0; i < size; i++) {
+        	for (int j = 0; j < size; j++) {
+            		printf("%d ", arr[i][j]);
+        	}
+        	puts("");
+    	}
+}
+
---
+++ HomeWorkLessonSeven/hw7_1.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+#include <memory.h>
+#include "homework7.h"
+
+/*
+ * 1. Написать функции, которые считывают матрицу смежности из файла и выводят ее на экран.
+ */
+
+int main(int argc, char* argv[]) {
+    	FILE* file = fopen("hw7_data.txt", "r");
+    	int size = 0;
+    	readFileToMatrix(file, &size);
+	fclose(file);	
+    	return 0;
+}
